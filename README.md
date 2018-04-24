2caac69de3d26409adac48a9307a5dc976a1f8b0
gzing <gagan.zingh@gmail.com>
2018-04-24 00:11:55 -0700

    Move to Flask+Heroku + minor improvements

--- README.md
+++ README.md
@@ -1,54 +1,25 @@
 # gjallarhorn
 
 Create a python3 virtual env
-`virtualenv -p python3 .chalice/env`
+`python3.6 -m venv venv/`
 
 
 Activate the created virtual env
-`. .chalice/env/bin/activate`
+`. ./venv/bin/activate`
 
 
 Install requirements
 `pip install -r requirements.txt`
 
 
-Create a configuration file at .chalice/config.json
-
+Set the following environment variables:
 ```
-{
-  "version": "2.0",
-  "app_name": "gjallarhorn",
-  "environment_variables": {
-    "WHITELIST_DOMAINS": Domain list split by commas "google.com,yahoo.com,localhost.,127.0.0.1.",
-    "FROM_EMAIL": "xyz@example.com",
-    "TO_EMAIL": "abc@example.com",
-    "SENDGRID_KEY": "sendgrid key",
-    "EMAIL_SUBJECT": "Subject of the email"
-  },
-  "stages": {
-    "dev": {
-      "api_gateway_stage": "api"
-    }
-  }
-}
+WHITELIST_DOMAINS (Domain list split by commas "google.com,yahoo.com,localhost.,127.0.0.1."),
+FROM_EMAIL (e.g. "xyz@example.com")
+TO_EMAIL (e.g. "abc@example.com")
+SENDGRID_KEY (sendgrid key)
+EMAIL_SUBJECT (Subject of the email)
 ```
-
-
-If this is your first time configuring credentials for AWS you can follow these steps to quickly get started:
-```
-$ mkdir ~/.aws
-$ cat >> ~/.aws/config
-[default]
-aws_access_key_id=YOUR_ACCESS_KEY_HERE
-aws_secret_access_key=YOUR_SECRET_ACCESS_KEY
-region=YOUR_REGION (such as us-west-2, us-west-1, etc)
-```
-
 
 To test locally:
-`chalice local`
-
-
-To deploy on aws:
-`chalice deploy`
-
+`FLASK_APP=app.py flask run`
--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from chalice import Chalice
-
 import os
+
+from flask import Flask, request, Response
+from flask_cors import CORS
 import sendgrid
 from sendgrid.helpers.mail import *
 import tldextract
 
-app = Chalice(app_name='gjallarhorn')
+
+app = Flask(__name__)
+CORS(app)
+
+BLANK_IMG = b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b'
 
 
-@app.route('/', methods=['GET'], cors=True)
-def index():
-    req_data = app.current_request.headers
-    referer = req_data.get('referer')
+@app.route('/', methods=['GET'])
+@app.route('/<filename>', methods=['GET'])
+def index(filename=None):
+    referer = request.headers.get('referer')
     if referer:
-        extract = '.'.join(tldextract.extract(referer)[1:])
+        extract = tldextract.extract(referer)
+        extract = extract.fqdn or extract.ipv4
         if extract not in os.environ['WHITELIST_DOMAINS'].split(','):
             try:
                 sg = sendgrid.SendGridAPIClient(apikey=os.environ['SENDGRID_KEY'])
                 from_email = Email(os.environ['FROM_EMAIL'])
                 to_email = Email(os.environ['TO_EMAIL'])
                 subject = os.environ['EMAIL_SUBJECT']
-                content = Content("text/plain", str(app.current_request.headers))
+                content = Content("text/plain", str(request.headers))
                 mail = Mail(from_email, subject, to_email, content)
                 sg.client.mail.send.post(request_body=mail.get())
             except Exception as e:
-                app.log.error(e)
-    return {"img": "not found"}
+                app.logger.error(e)
+    return Response(BLANK_IMG, mimetype='image/gif')
